e5bb58a6efd69e054dea475bc88d9f26d8e15b5d
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-10-03 18:56:19 -0300

    create node class

---
+++ lib/classes/node.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Paulo Ricardo
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+class Node {
+  int value;
+  Node? left, right;
+
+  Node({required this.value});
+}
